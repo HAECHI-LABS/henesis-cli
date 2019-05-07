8e9c2ff76eb6f9e7900e1c7ef70a3f53fa7c27cb
jun <leebduk@gmail.com>
2019-05-08 01:00:21 +0900

    refactor: change promise to await/async

---
+++ src/rpc/config.ts
@@ -0,0 +1,2 @@
+export const baseUrl = 'http://localhost:8080';
+export const rpcVersion = 'v1';
--- src/rpc/integration.spec.ts
+++ src/rpc/integration.spec.ts
@@ -3,62 +3,109 @@ import { IntegrationRpc } from './integration';
 import { ChainType, Handler, Integration, Subscriber, Webhook } from '../types';
 import * as faker from 'faker';
 import wretch from 'wretch';
+import { baseUrl, rpcVersion } from './config';
 
 wretch().polyfills({
-    fetch: require("node-fetch"),
-    FormData: require("form-data"),
-    URLSearchParams: require("url").URLSearchParams
+  fetch: require('node-fetch'),
+  FormData: require('form-data'),
+  URLSearchParams: require('url').URLSearchParams,
 });
 
 const mockServer = require('mockttp').getLocal();
 
 describe('IntegrationRpc', () => {
   let integrationRpc: IntegrationRpc;
-  // Start your server
+  // Start your servers
   beforeEach(async () => {
-    integrationRpc = new IntegrationRpc('http://localhost:8080');
+    integrationRpc = new IntegrationRpc(baseUrl, rpcVersion);
     await mockServer.start(8080);
   });
-  
+
   afterEach(async () => {
     await mockServer.stop();
   });
-  
-  it('should get integration list', async () => {
-    const integrations = [mockIntegrationFactory(), mockIntegrationFactory()];
-    await mockServer.get('/integrations/v1').thenJSON(200, integrations);
-    const response: Integration[] = await integrationRpc.getIntegrationList();
-    expect(response).to.deep.equal(integrations);
+
+  describe('#getIntegrations()', async () => {
+    it('should get integration list', async () => {
+      const integrations = [mockIntegrationFactory(), mockIntegrationFactory()];
+      await mockServer.get('/integrations/v1').thenJSON(200, integrations);
+      const response: Integration[] = await integrationRpc.getIntegrations();
+      expect(response).to.deep.equal(integrations);
+    });
+
+    it('should throw invalid response format error', async () => {
+      const invalidJson = { invalid: 'error' };
+      await mockServer.get('/integrations/v1').thenJSON(200, invalidJson);
+      try {
+        await integrationRpc.getIntegrations();
+      } catch (err) {
+        expect(err.toString()).to.equal(
+          'Error: Expected getIntegrationsList to return an array but it returned [object Object]',
+        );
+      }
+    });
+
+    it('should throw 400 httpError', async () => {
+      const res = 'page not found';
+      await mockServer.get('/integrations/v1').thenReply(400, res);
+      try {
+        await integrationRpc.getIntegrations();
+      } catch (err) {
+        expect(err.toString()).to.deep.equal('Error: page not found');
+      }
+    });
   });
-  
-  it('should get a integration', async () => {
-    const integration = mockIntegrationFactory();
-    await mockServer.get('/integrations/v1/' + integration.integrationId).thenJSON(200, integration);
-    const response: Integration | null = await integrationRpc.getIntegration(integration.integrationId);
-    expect(response).to.deep.equal(integration);
+
+  describe('#getIntegration()', async () => {
+    it('should get a integration', async () => {
+      const integration = mockIntegrationFactory();
+      await mockServer
+        .get('/integrations/v1/' + integration.integrationId)
+        .thenJSON(200, integration);
+      const response: Integration | null = await integrationRpc.getIntegration(
+        integration.integrationId,
+      );
+      expect(response).to.deep.equal(integration);
+    });
   });
-  
-  it('should patch a integration', async () => {
-    const integration = mockIntegrationFactory();
-    integration.version = 'v3';
-    
-    await mockServer.patch('/integrations/v1/' + integration.integrationId).thenJSON(200, integration);
-    const response: Integration | null = await integrationRpc.patchIntegration(integration.integrationId, { version: 'v3' } as Integration);
-    expect(response).to.deep.equal(integration);
+
+  describe('#patchIntegration()', async () => {
+    it('should patch a integration', async () => {
+      const integration = mockIntegrationFactory();
+      integration.version = 'v3';
+      await mockServer
+        .patch('/integrations/v1/' + integration.integrationId)
+        .thenJSON(200, integration);
+      const response: Integration | null = await integrationRpc.patchIntegration(
+        integration.integrationId,
+        { version: 'v3' } as Integration,
+      );
+      expect(response).to.deep.equal(integration);
+    });
   });
-  
-  it('should create a integration', async () => {
-    const integration = mockIntegrationFactory();
-    await mockServer.post('/integrations/v1').thenJSON(200, integration);
-    const response: Integration | null = await integrationRpc.createIntegration(integration);
-    expect(response).to.deep.equal(integration);
+
+  describe('#createIntegration()', async () => {
+    it('should create a integration', async () => {
+      const integration = mockIntegrationFactory();
+      await mockServer.post('/integrations/v1').thenJSON(200, integration);
+      const response: Integration | null = await integrationRpc.createIntegration(
+        integration,
+      );
+      expect(response).to.deep.equal(integration);
+    });
   });
-  
-  it('should delete a integration', async () => {
-    const integration = mockIntegrationFactory();
-    await mockServer.delete('/integrations/v1/' + integration.integrationId).thenReply(200, null);
-    const response: null = await integrationRpc.deleteIntegration(integration.integrationId);
-    expect(response).to.deep.equal(null);
+
+  describe('#deleteIntegration()', async () => {
+    it('should delete a integration', async () => {
+      const integration = mockIntegrationFactory();
+      await mockServer
+        .delete('/integrations/v1/' + integration.integrationId)
+        .thenReply(200, null);
+      const response: null = await integrationRpc.deleteIntegration(
+        integration.integrationId,
+      );
+      expect(response).to.deep.equal(null);
+    });
   });
 });
 
@@ -69,25 +116,25 @@ export function mockIntegrationFactory(): Integration {
     faker.name.firstName(),
     'v1',
     new Subscriber(
-      "localhost:8080",
-      "0x12u31ijdiasdjmi",
-      "abi",
+      'localhost:8080',
+      '0x12u31ijdiasdjmi',
+      'abi',
       ChainType.ETHEREUM,
     ),
-    [new Handler(
-      faker.random.alphaNumeric(),
-      faker.name.firstName(),
-      "event",
-      "v1",
-      faker.random.words(5),
-      "tsnode8",
-      "handler"
-    )],
-    new Webhook(
-      faker.internet.ip(),
-      "POST",
-      {"Authorization": "Bear ashd8uado9012i31kod"},
-    ),
-    'Starting'
-  )
+    [
+      new Handler(
+        faker.random.alphaNumeric(),
+        faker.name.firstName(),
+        'event',
+        'v1',
+        faker.random.words(5),
+        'tsnode8',
+        'handler',
+      ),
+    ],
+    new Webhook(faker.internet.ip(), 'POST', {
+      Authorization: 'Bear ashd8uado9012i31kod',
+    }),
+    'Starting',
+  );
 }

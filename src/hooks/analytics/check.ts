7222cdffef048c0bce3409e9197c2fb5ce0f3ca3
Choi Yoons <ys.choi@me.com>
2019-05-10 13:49:45 +0900

    Add Analytics Check

---
+++ src/hooks/analytics/check.ts
@@ -0,0 +1,18 @@
+import { Hook } from '@oclif/config';
+import { cli } from 'cli-ux';
+import configstore from '../../configstore';
+
+const hook: Hook<'prerun'> = async function({}): Promise<void> {
+  const isAgree = configstore.get('analytics');
+
+  if (isAgree || isAgree === false) return;
+
+  cli.log(
+    `Allow Henesis to collect anonymous CLI usage and error reporting information`,
+  );
+  let input = await cli.confirm(`yes(y) or no(n)`);
+
+  configstore.set('analytics', input);
+};
+
+export default hook;

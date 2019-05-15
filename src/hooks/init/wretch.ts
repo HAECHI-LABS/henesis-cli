0b2536a16b6a9f039ba089077b73922ca80a8437
Choi Yoons <ys.choi@me.com>
2019-05-15 15:26:54 +0900

    feat: update hook.

--- src/hooks/init/wretch.ts
+++ src/hooks/init/wretch.ts
@@ -1,13 +1,27 @@
 import { Hook } from '@oclif/config';
 import wretch from 'wretch';
+import configstore from '../../configstore';
 
 // set for nodejs
 const hook: Hook<'init'> = async function(opts) {
-  wretch().polyfills({
-    fetch: require('node-fetch'),
-    FormData: require('form-data'),
-    URLSearchParams: require('url').URLSearchParams,
-  });
+  const isExistUser = configstore.has('user');
+
+  if (isExistUser) {
+    const user = configstore.get('user');
+    wretch()
+      .polyfills({
+        fetch: require('node-fetch'),
+        FormData: require('form-data'),
+        URLSearchParams: require('url').URLSearchParams,
+      })
+      .auth(`Bearer ${user.jwtToken}`);
+  } else {
+    wretch().polyfills({
+      fetch: require('node-fetch'),
+      FormData: require('form-data'),
+      URLSearchParams: require('url').URLSearchParams,
+    });
+  }
 };
 
 export default hook;

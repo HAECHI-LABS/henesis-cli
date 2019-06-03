93fcf4ba8d3838e1c3ec0b0381834611c3c9d410
Choi Yoons <ys.choi@me.com>
2019-06-03 17:51:49 +0900

    mod: template upgrade

--- templates/handlers/execution2.ts
+++ templates/handlers/execution2.ts
@@ -1,6 +1,11 @@
 import { Ethereum } from '@haechi-labs/henesis-sdk';
 
-exports.execution2 = (web3: any, data: Ethereum.Data): any => {
-  console.log('event', data);
-  return data;
+exports.execution2 = (
+  web3: any,
+  event: Ethereum.Data,
+  blockMeta: Ethereum.BlockMeta,
+  userMeta: Ethereum.UserMeta,
+): any => {
+  console.log('event', event.payload);
+  return event.payload;
 };
--- templates20/handlers/ERC20-approval.ts
+++ templates20/handlers/ERC20-approval.ts
@@ -1,6 +1,11 @@
 import { Ethereum } from '@haechi-labs/henesis-sdk';
 
-exports.approval = (web3: any, data: Ethereum.Data): any => {
-  console.log('event', data);
-  return data;
+exports.approval = (
+  web3: any,
+  event: Ethereum.Data,
+  blockMeta: Ethereum.BlockMeta,
+  userMeta: Ethereum.UserMeta,
+): any => {
+  console.log('event', event.payload);
+  return event.payload;
 };

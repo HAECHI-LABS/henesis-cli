7cb8747fa452843a7ea6ae3f3e29cd6b411f1864
Choi Yoons <ys.choi@me.com>
2019-06-04 17:44:01 +0900

    mod: dashboard link

--- src/commands/integration/status.ts
+++ src/commands/integration/status.ts
@@ -47,8 +47,8 @@ export default class Status extends Command {
     try {
       const integrations = await integrationRpc.getIntegrations();
 
-      cli.url('View Dashboard,', 'https://dashbard.henesis.io');
-      this.log('You can login with the same email and password as henesis.');
+      cli.url('View Dashboard', 'https://dashboard.henesis.io');
+      this.log('└─ You can login with the same email and password as henesis.');
 
       cli.table(integrations, columns, {
         printLine: this.log,

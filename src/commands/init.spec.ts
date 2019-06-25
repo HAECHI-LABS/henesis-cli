de876c3407a193a1e003c323358d9e81d6ad0104
Choi Yoons <ys.choi@me.com>
2019-06-25 17:56:20 +0900

    feat: git clone use template.

--- src/commands/init.spec.ts
+++ src/commands/init.spec.ts
@@ -34,6 +34,20 @@ describe('init', (): void => {
 
   test
     .stdout()
+    .command(['init', '-g', 'https://github.com/HAECHI-LABS/henesis-dai.git'])
+    .it(
+      'should be able to successfully create folders with git template',
+      (ctx): void => {
+        const path = join(__dirname, '..', '..', `/henesis-dai`);
+        rimraf(path, (): void => {});
+        expect(ctx.stdout).to.equal(
+          'henesis-dai directory has been created.\n',
+        );
+      },
+    );
+
+  test
+    .stdout()
     .command(['init'])
     .command(['init'])
     .exit(2)

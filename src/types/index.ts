d9db313025f40459694e915dd706a78ce412c945
Jun <leebduk@gmail.com>
2019-08-22 18:23:10 +0900

    feat: update integration type and status command

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,8 +2,6 @@ export {
   PlatformType,
   NetworkType,
   Integration,
-  Handler,
-  Webhook,
 } from './Integration';
 export { IntegrationSpec, HandlerSpec, WebhookSpec } from './integrationSpec';
 export { LoginRequest, LoginResponse, ChangePassword } from './user';

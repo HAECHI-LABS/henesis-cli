8b78a7b69dff8918dbd8b3af4bf46cf49daea993
Jun <leebduk@gmail.com>
2019-08-27 15:26:40 +0900

    refactor: add default error catcher and refine rpc error message

--- src/hooks/init/wretch.ts
+++ src/hooks/init/wretch.ts
@@ -1,24 +1,18 @@
 import { Hook } from '@oclif/config';
-import wretch from 'wretch';
 import configstore from '../../common/configstore';
-import { setWretcher } from '../../rpc/wretch';
+import { getWretcher, setWretcher } from '../../rpc/wretch';
 
 // set for nodejs
 const hook: Hook<'init'> = async function(opts) {
   const isExistUser = configstore.has('user');
-
   if (isExistUser) {
     const user = configstore.get('user');
     setWretcher(
-      wretch()
-        .polyfills({
-          fetch: require('node-fetch'),
-          FormData: require('form-data'),
-          URLSearchParams: require('url').URLSearchParams,
-        })
+      getWretcher()
         .auth(`Bearer ${user.jwtToken}`)
         .catcher(401, () => {
           configstore.delete('user');
+          throw new Error('expired token (please log in again)');
         }),
     );
   }
--- src/rpc/wretch.ts
+++ src/rpc/wretch.ts
@@ -1,10 +1,27 @@
-import wretch, { Wretcher } from 'wretch';
+import wretch, { Wretcher, WretcherError } from 'wretch';
+import configstore from '../common/configstore';
 
-let wretchInstance: Wretcher = wretch().polyfills({
-  fetch: require('node-fetch'),
-  FormData: require('form-data'),
-  URLSearchParams: require('url').URLSearchParams,
-});
+const defaultErrorCatcher = (
+  error: WretcherError,
+  originalRequest: Wretcher,
+): any => {
+  if (error.json === undefined || error.json.error === undefined) {
+    throw new Error('invalid response format');
+  }
+  throw new Error(error.json.error.message);
+};
+
+let wretchInstance: Wretcher = wretch()
+  .errorType('json')
+  .catcher(400, defaultErrorCatcher)
+  .catcher(404, defaultErrorCatcher)
+  .catcher(500, defaultErrorCatcher)
+  .catcher(401, defaultErrorCatcher)
+  .polyfills({
+    fetch: require('node-fetch'),
+    FormData: require('form-data'),
+    URLSearchParams: require('url').URLSearchParams,
+  });
 
 export const setWretcher = (wretcher: Wretcher): void => {
   wretchInstance = wretcher;
